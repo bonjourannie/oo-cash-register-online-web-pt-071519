23ce71fd16c0339017b8a11bbf51e047839c582a
Annie Truex <artruex@gmail.com>
2019-08-15 00:06:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    self.total += amount * quantity
+    self.total += price * quantity
     quantity.times do
       items << title
     end
